5ac3fdac60ae11036a57e06d044419ba771677b8
Józef Podlecki <jozef.witold.podlecki@gmail.com>
2025-02-25 23:16:17 +0000

    fix

--- app/src/handlers/mod.rs
+++ app/src/handlers/mod.rs
@@ -1,5 +1,5 @@
 
-use tauri::{generate_context, generate_handler, AppHandle, Manager};
+use tauri::{generate_handler, AppHandle, Manager};
 
 pub fn generate_handlers() -> Box<dyn Fn(tauri::ipc::Invoke) -> bool + Send + Sync> {
     Box::new(generate_handler![
